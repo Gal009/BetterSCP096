0d6dd8220945476d4a1efaefe87dc2028887af19
Gal009 <92556891+Gal009@users.noreply.github.com>
2022-11-19 01:25:48 -0300

    Last commit

---
+++ BetterSCP096/Patches/RageEndPatch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using PlayableScps;
+
+namespace BetterSCP096.Patches
+{
+    [HarmonyPatch(typeof(Scp096))]
+    [HarmonyPatch(nameof(Scp096.ResetShield))]
+    internal static class RageEndPatch
+    {
+        static bool Prefix(Scp096 __instance)
+        {
+            return false;
+        }
+    }
+}
